1f1c2f8d5809502c7ca4b567aa830cb569e781a2
enigmaks72 <160804797+enigmaks72@users.noreply.github.com>
2024-02-22 16:31:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ from loguru import logger
 
 from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
-# from langchain_google_genai import ChatGoogleGenerativeAI
 
-# loaders
 from langchain.document_loaders import PyPDFLoader
 from langchain.document_loaders import Docx2txtLoader
 from langchain.document_loaders import UnstructuredPowerPointLoader
