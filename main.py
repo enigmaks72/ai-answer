9410e1c7a5f1265040514d74d6d0eb5a1f00eede
enigmaks72 <160804797+enigmaks72@users.noreply.github.com>
2024-02-22 16:37:51 +0900

    Update main.py
    line : 75
    question -> query 변경

--- main.py
+++ main.py
@@ -72,7 +72,9 @@ def main():
             chain = st.session_state.conversation
 
             with st.spinner("생각중입니다..."):
-                result = chain({"question": query})
+                result = chain({"query": query})
+     #          result = chain({"question": query})
+
                 with get_openai_callback() as cb:
                     st.session_state.chat_history = result['chat_history']
                 response = result['answer']
