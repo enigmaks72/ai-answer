c528aa20a5761ea48ae9070c784f2cf23a84e349
enigmaks72 <160804797+enigmaks72@users.noreply.github.com>
2024-04-01 11:19:54 +0900

    Update main.py
    Title Changed

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def main():
     page_title="DirChat",
     page_icon=":books:")
 
-    st.title("_Onion Private Data :red[QA Chat]_ :books:")
+    st.title("_Onion Private Data :red[AI QA Chat]_ :books:")
 
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
